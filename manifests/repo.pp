0983972ea9d6bb0a6bf0523b46048181a8c8cf30
Jakob Schlyter <jakob@kirei.se>
2015-10-02 13:48:45 +0200

    add repo key

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,6 +1,12 @@
 # IPnett BaaS repository
 class ipnett_baas::repo {
+
   file {
+    '/etc/pki/rpm-gpg/RPM-GPG-KEY-IPnett':
+      ensure  => file,
+      mode    => '0444',
+      source => 'puppet:///modules/ipnett_baas/RPM-GPG-KEY-IPnett';
+
     '/etc/yum.repos.d/ipnett-baas.repo':    
       ensure  => file,
       mode    => '0444',
