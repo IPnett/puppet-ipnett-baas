73c0571da0f9825a92fc4f7185502b8d9be7c15f
Jakob Schlyter <jakob@kirei.se>
2015-10-02 15:06:31 +0200

    validate

--- manifests/enroll.pp
+++ manifests/enroll.pp
@@ -1,28 +1,42 @@
 # IPnett BaaS enroller
 class ipnett_baas::enroll (
 
+  # base64-encoded access token, e.g., created using
+  # echo -n $access_key:$secret_key | openssl enc -e -base64
   $access_token      = undef,
-  $application       = 'filesystem',
-  $compression       = true,
-  $costcenter        = '1000',
-  $deduplication     = true,
-  $encryption        = false,
+
   $hostname          = $::fqnd,
   $host_description  = $::fqdn,
   $mail_address      = undef,
+  $costcenter        = '1000',
+
+  $compression       = true,
+  $deduplication     = true,
+  $encryption        = false,
+
   $platform          = undef,
+  $application       = 'filesystem',
 
 )
 {
   validate_string($access_token)
-  validate_string($application)
-  validate_bool($compression)
-  validate_string($costcenter)
-  validate_bool($deduplication)
-  validate_bool($encryption)
+
   validate_string($hostname)
   validate_string($host_description)
   validate_string($mail_address)
+  validate_string($costcenter)
+
+  validate_bool($compression)
+  validate_bool($deduplication)
+  validate_bool($encryption)
+
+  if ($platform {
+    validate_string($platform)
+  } else {
+    # magic
+  }
+
+  validate_string($application)
 
   package { 'ipnett-baas-setup':
     ensure => 'latest';
