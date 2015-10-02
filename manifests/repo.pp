92928f9ff3283eedc327172f0fb61d0f27e81374
Jakob Schlyter <jakob@kirei.se>
2015-10-02 14:38:53 +0200

    select distribution

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,18 +1,30 @@
 # IPnett BaaS repository
-class ipnett_baas::repo (
+class ipnett_baas::repo
+{
+  case $::osfamily {
+    'redhat': {
+      $major = $::os['release']['major']
+      case $major {
+        '6': { $distribution = 'el6' }
+        '7': { $distribution = 'el7' }
+        default: { fail("Unsupported RedHat release ${major}") }
+      }
 
-  $distribution = 'el7',
+      file {
+        '/etc/pki/rpm-gpg/RPM-GPG-KEY-IPnett':
+          ensure  => file,
+          mode    => '0444',
+          source => 'puppet:///modules/ipnett_baas/RPM-GPG-KEY-IPnett';
 
-){
-  file {
-    '/etc/pki/rpm-gpg/RPM-GPG-KEY-IPnett':
-      ensure  => file,
-      mode    => '0444',
-      source => 'puppet:///modules/ipnett_baas/RPM-GPG-KEY-IPnett';
+        '/etc/yum.repos.d/ipnett-baas.repo':
+          ensure  => file,
+          mode    => '0444',
+          content => template('ipnett_baas/ipnett-baas.repo.erb');
+      }
 
-    '/etc/yum.repos.d/ipnett-baas.repo':
-      ensure  => file,
-      mode    => '0444',
-      content => template('ipnett_baas/ipnett-baas.repo.erb');
+    }
+    default: {
+      fail("Unsupported osfamily ${::osfamily}")
+    }
   }
 }
